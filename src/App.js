50762ac8fb6010bbe5f9887e03f22ec80faaac80
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-06-14 14:32:53 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Header } from './components/Header/Header';
+import { Level } from './components/Level/Level';
+import { Classe } from './components/Classe/Classe';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<Header></Header>
+			<Routes>
+				<Route path='/level' element={<Level></Level>}></Route>
+				<Route path='/classe' element={<Classe></Classe>}></Route> 
+			</Routes>
+		</React.Fragment>
+	);
 }
 
 export default App;
